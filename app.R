05b046318f661623fab3ea33a75553c4d67fd2ae
Michael Yun <xfy5111@psu.edu>
2024-07-16 21:55:00 +0800

    Update app.R

--- app.R
+++ app.R
@@ -30,8 +30,10 @@ ui <- list(
       ),
       tags$li(
         class = "dropdown",
-        tags$a(href = 'https://shinyapps.science.psu.edu/',
-               icon("house")
+        tags$a(
+          id = "home",
+          href = 'https://shinyapps.science.psu.edu/',
+          icon("house")
         )
       )
     ),
